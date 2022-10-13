e32db1423e28504af6c6af11183210991a4ee3d1
Akinfola <ejideayodele@gmail.com>
2022-10-13 15:41:52 -0700

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
  *  *main- Serves as the entry point for the program
  *   *
@@ -9,4 +9,3 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
