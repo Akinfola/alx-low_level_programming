1917ad3f5bd529646f5da3d80d3e523afc233e14
Akinfola <ejideayodele@gmail.com>
2022-11-04 15:34:30 -0700

    task one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
-#include "main.h"
-/**
- * main - print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- * Return: Always 0 (Success)
+/*
+ * File: 1-args.c
+ * Auth: Gedeon Obae Gekonge
  */
-int main(int argc, char *argv[])
+
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed to it.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	(void) argv; /*Ignore argv*/
-	printf("%i\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
